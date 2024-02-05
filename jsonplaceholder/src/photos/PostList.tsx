dcc53846cdd51bafa2ee3c4293872aeb1b9437ee
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-05 22:33:09 +0600

    filtering

--- jsonplaceholder/src/photos/PostList.tsx
+++ jsonplaceholder/src/photos/PostList.tsx
@@ -1,7 +1,17 @@
-import { List, ListItem, ListItemText } from '@mui/material'
+import {
+  FormControl,
+  InputLabel,
+  List,
+  ListItem,
+  ListItemText,
+  MenuItem,
+  Select,
+} from '@mui/material'
+import { useState } from 'react'
 import usePosts from '../hooks/usePosts'
 
 const PostList = () => {
+  const [userId, setUserTd] = useState<number>()
   const { data: posts, error, isLoading } = usePosts()
 
   if (error) return <p>{error.message}</p>
@@ -9,6 +19,20 @@ const PostList = () => {
 
   return (
     <div>
+      <FormControl fullWidth>
+        <InputLabel id='user'>user</InputLabel>
+        <Select
+          labelId='user'
+          id='user'
+          value={userId}
+          onChange={(event) => setUserTd(parseInt(event.target.value))}
+        >
+          <MenuItem value=''>User</MenuItem>
+          <MenuItem value='1'>User 1</MenuItem>
+          <MenuItem value='2'>User 2</MenuItem>
+          <MenuItem value='3'>User 3</MenuItem>
+        </Select>
+      </FormControl>
       <List>
         {posts?.map((post) => (
           <ListItem key={post.id} sx={{ borderBottom: 1 }}>
