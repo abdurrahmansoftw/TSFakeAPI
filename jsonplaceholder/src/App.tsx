226be7cdb5c69223da26c987644ece064172f2a5
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-05 21:58:44 +0600

    RemoveNavbar

--- jsonplaceholder/src/App.tsx
+++ jsonplaceholder/src/App.tsx
@@ -1,12 +1,10 @@
 import { Container } from '@mui/material'
 import React from 'react'
-import Navbar from './components/Navbar'
 import PhotoList from './photos/PhotoList'
 
 const App = () => {
   return (
     <React.Fragment>
-      <Navbar />
       <Container>
         <h1 className='text-3xl font-bold  my-4'>TodoList: React Queary!</h1>
         <PhotoList />
