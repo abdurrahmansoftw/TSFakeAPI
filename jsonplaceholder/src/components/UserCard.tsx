82f091b9f348d5965c8050d7768d67548fd24226
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-05 23:18:21 +0600

    UserCard

--- jsonplaceholder/src/components/UserCard.tsx
+++ jsonplaceholder/src/components/UserCard.tsx
@@ -2,15 +2,26 @@ import { Card, CardContent, Typography, styled } from '@mui/material'
 import { User } from '../hooks/useUsers'
 
 const CustomCardContainer = styled(Card)`
-  background-color: #f0f0f0;
-  border: 2px solid #e0e0e0;
-  border-radius: 8px;
-  padding: 4px;
-  transition: background-color 0.3s ease;
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  transition: box-shadow 0.3s ease-in-out;
 
   &:hover {
-    background-color: #e0e0e0;
+    box-shadow: 0px 6px 20px rgba(0, 0, 0, 0.2);
   }
+`
+
+const CardHeader = styled('div')`
+  background-color: #1976d2;
+  color: #ffffff;
+  padding: 16px;
+  border-radius: 10px 10px 0 0;
+`
+
+const CustomCardContent = styled(CardContent)`
+  padding: 16px;
 `
 
 interface Users {
@@ -20,28 +31,30 @@ interface Users {
 const UserCard = ({ user }: Users) => {
   return (
     <CustomCardContainer>
-      <CardContent>
-        <Typography variant='h5'>{user.name}</Typography>
-        <Typography variant='body2' color='text.secondary'>
+      <CardHeader>
+        <Typography variant='h6'>{user.name}</Typography>
+      </CardHeader>
+      <CustomCardContent>
+        <Typography variant='body2' color='textSecondary'>
           Username: {user.username}
         </Typography>
-        <Typography variant='body2' color='text.secondary'>
+        <Typography variant='body2' color='textSecondary'>
           Email: {user.email}
         </Typography>
-        <Typography variant='body2' color='text.secondary'>
+        <Typography variant='body2' color='textSecondary'>
           Phone: {user.phone}
         </Typography>
-        <Typography variant='body2' color='text.secondary'>
-          Address: {user.address.street}, {user.address.suite},
+        <Typography variant='body2' color='textSecondary'>
+          Address: {user.address.street}, {user.address.suite},{' '}
           {user.address.city}, {user.address.zipcode}
         </Typography>
-        <Typography variant='body2' color='text.secondary'>
+        <Typography variant='body2' color='textSecondary'>
           Company: {user.company.name}
         </Typography>
-        <Typography variant='body2' color='text.secondary'>
+        <Typography variant='body2' color='textSecondary'>
           Website: {user.website}
         </Typography>
-      </CardContent>
+      </CustomCardContent>
     </CustomCardContainer>
   )
 }
