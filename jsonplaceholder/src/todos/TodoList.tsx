bf334ad8efd040c6ec4f4786899c66bf08c2429a
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-05 20:18:04 +0600

    useTodos

--- jsonplaceholder/src/todos/TodoList.tsx
+++ jsonplaceholder/src/todos/TodoList.tsx
@@ -6,34 +6,13 @@ import {
   ListItem,
   ListItemText,
 } from '@mui/material'
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
-
-interface Todo {
-  id: number
-  title: string
-  userId: number
-  completed: boolean
-}
+import useTodos from '../hooks/useTodos'
 
 const TodoList = () => {
-  const fetchTodos = () =>
-    axios
-      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
-      .then((response) => response.data)
-
-  const {
-    data: todos,
-    error,
-    isLoading,
-  } = useQuery<Todo[], Error>({
-    queryKey: ['todos'],
-    queryFn: fetchTodos,
-  })
-
+  const { data: todos, error, isLoading } = useTodos()
   if (error) return <p>{error.message}</p>
   if (isLoading) return <CircularProgress />
-  
+
   return (
     <Box sx={{ width: '100%' }}>
       <List>
