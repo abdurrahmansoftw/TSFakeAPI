8101eb85ff74b87766c9f31f34ed6dbca5b2d478
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-05 19:27:39 +0600

    TodoList Add todo interface

--- jsonplaceholder/src/todos/TodoList.tsx
+++ jsonplaceholder/src/todos/TodoList.tsx
@@ -1,4 +1,14 @@
+import { useState } from 'react'
+
+interface Todo {
+  id: number
+  title: string
+  userId: number
+  completed: boolean
+}
+
 const TodoList = () => {
+  const [todos, setTodos] = useState<Todo[]>([])
   return <div>TodoList</div>
 }
 
