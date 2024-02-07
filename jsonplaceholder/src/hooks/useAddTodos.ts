0e40ca6199173d75cb2986103e408c2867bc5ed5
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-07 23:08:06 +0600

    useAddTodos

--- jsonplaceholder/src/hooks/useAddTodos.ts
+++ jsonplaceholder/src/hooks/useAddTodos.ts
@@ -1,9 +1,43 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import todoService from '../services/todoService'
+
+import { CACHE_KEY_TODOS } from './constants'
 import { Todo } from './useTodos'
 
 interface AddTodoContext {
   previusTodos: Todo[]
 }
 
-const useAddTodos = () => {}
+const useAddTodos = (onAdd: () => void) => {
+  const queryClient = useQueryClient()
+
+  return useMutation<Todo, Error, Todo, AddTodoContext>({
+    mutationFn: todoService.post,
+
+    onMutate: (newTodo: Todo) => {
+      const previusTodos =
+        queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || []
+
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos = []) => [
+        newTodo,
+        ...todos,
+      ])
+
+      onAdd()
+      return { previusTodos }
+    },
+
+    onSuccess: (savedTodo, newTodo) => {
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      )
+    },
+
+    onError: (error, newTodo, context) => {
+      if (!context) return
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, context?.previusTodos)
+    },
+  })
+}
 
 export default useAddTodos
