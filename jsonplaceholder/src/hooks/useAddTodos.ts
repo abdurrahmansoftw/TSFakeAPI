232c4bf27b89f4b488174797fae31416df07c8a9
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-07 23:06:14 +0600

    apiClient.ts

--- jsonplaceholder/src/hooks/useAddTodos.ts
+++ jsonplaceholder/src/hooks/useAddTodos.ts
@@ -1,5 +1,9 @@
-const useAddTodos = () => {
-  
+import { Todo } from './useTodos'
+
+interface AddTodoContext {
+  previusTodos: Todo[]
 }
+
+const useAddTodos = () => {}
 
 export default useAddTodos
