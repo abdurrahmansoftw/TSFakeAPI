7065f59fcccec2a87d0c6e618d727faf44ef8ff9
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-07 22:23:31 +0600

    form

--- jsonplaceholder/src/todos/TodoList.tsx
+++ jsonplaceholder/src/todos/TodoList.tsx
@@ -1,14 +1,21 @@
-import { List, ListItem } from '@mui/material'
+import { Box, Button, List, ListItem, Stack, TextField } from '@mui/material'
 import useTodos from '../hooks/useTodos'
 
 const TodoList = () => {
   const { data: todos } = useTodos()
   return (
-    <List>
-      {todos?.map((todo) => (
-        <ListItem>{todo.title}</ListItem>
-      ))}
-    </List>
+    <Stack>
+      <Box component={'form'}>
+        <TextField label='add' name='add' size='small' />
+        <Button variant='outlined'>Add</Button>
+      </Box>
+
+      <List>
+        {todos?.map((todo) => (
+          <ListItem>{todo.title}</ListItem>
+        ))}
+      </List>
+    </Stack>
   )
 }
 
