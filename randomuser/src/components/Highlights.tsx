26ad7886a56f7000a9d0ab0d81a02d5e537ee550
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-20 19:54:49 +0600

    Highlights update

--- randomuser/src/components/Highlights.tsx
+++ randomuser/src/components/Highlights.tsx
@@ -1,34 +1,26 @@
-import AutoFixHighRoundedIcon from '@mui/icons-material/AutoFixHighRounded';
-import ConstructionRoundedIcon from '@mui/icons-material/ConstructionRounded';
-import QueryStatsRoundedIcon from '@mui/icons-material/QueryStatsRounded';
-import SettingsSuggestRoundedIcon from '@mui/icons-material/SettingsSuggestRounded';
-import SupportAgentRoundedIcon from '@mui/icons-material/SupportAgentRounded';
-import ThumbUpAltRoundedIcon from '@mui/icons-material/ThumbUpAltRounded';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
+import AutoFixHighRoundedIcon from '@mui/icons-material/AutoFixHighRounded'
+import ConstructionRoundedIcon from '@mui/icons-material/ConstructionRounded'
+import QueryStatsRoundedIcon from '@mui/icons-material/QueryStatsRounded'
+import SettingsSuggestRoundedIcon from '@mui/icons-material/SettingsSuggestRounded'
+import SupportAgentRoundedIcon from '@mui/icons-material/SupportAgentRounded'
+import ThumbUpAltRoundedIcon from '@mui/icons-material/ThumbUpAltRounded'
+import { Box, Card, Container, Grid, Stack, Typography } from '@mui/material'
 
 const items = [
   {
     icon: <SettingsSuggestRoundedIcon />,
     title: 'Adaptable performance',
-    description:
-      'Our product effortlessly adjusts to your needs, boosting efficiency and simplifying your tasks.',
+    description: 'Our product effortlessly adjusts to your needs, boosting efficiency and simplifying your tasks.',
   },
   {
     icon: <ConstructionRoundedIcon />,
     title: 'Built to last',
-    description:
-      'Experience unmatched durability that goes above and beyond with lasting investment.',
+    description: 'Experience unmatched durability that goes above and beyond with lasting investment.',
   },
   {
     icon: <ThumbUpAltRoundedIcon />,
     title: 'Great user experience',
-    description:
-      'Integrate our product into your routine with an intuitive and easy-to-use interface.',
+    description: 'Integrate our product into your routine with an intuitive and easy-to-use interface.',
   },
   {
     icon: <AutoFixHighRoundedIcon />,
@@ -39,8 +31,7 @@ const items = [
   {
     icon: <SupportAgentRoundedIcon />,
     title: 'Reliable support',
-    description:
-      'Count on our responsive customer support, offering assistance that goes beyond the purchase.',
+    description: 'Count on our responsive customer support, offering assistance that goes beyond the purchase.',
   },
   {
     icon: <QueryStatsRoundedIcon />,
@@ -48,12 +39,12 @@ const items = [
     description:
       'Enjoy a meticulously crafted product where small touches make a significant impact on your overall experience.',
   },
-];
+]
 
 export default function Highlights() {
   return (
     <Box
-      id="highlights"
+      id='highlights'
       sx={{
         pt: { xs: 4, sm: 12 },
         pb: { xs: 8, sm: 16 },
@@ -76,21 +67,20 @@ export default function Highlights() {
             textAlign: { sm: 'left', md: 'center' },
           }}
         >
-          <Typography component="h2" variant="h4">
+          <Typography component='h2' variant='h4'>
             Highlights
           </Typography>
-          <Typography variant="body1" sx={{ color: 'grey.400' }}>
-            Explore why our product stands out: adaptability, durability,
-            user-friendly design, and innovation. Enjoy reliable customer support and
-            precision in every detail.
+          <Typography variant='body1' sx={{ color: 'grey.400' }}>
+            Explore why our product stands out: adaptability, durability, user-friendly design, and innovation. Enjoy
+            reliable customer support and precision in every detail.
           </Typography>
         </Box>
         <Grid container spacing={2.5}>
           {items.map((item, index) => (
             <Grid item xs={12} sm={6} md={4} key={index}>
               <Stack
-                direction="column"
-                color="inherit"
+                direction='column'
+                color='inherit'
                 component={Card}
                 spacing={1}
                 useFlexGap
@@ -105,10 +95,10 @@ export default function Highlights() {
               >
                 <Box sx={{ opacity: '50%' }}>{item.icon}</Box>
                 <div>
-                  <Typography fontWeight="medium" gutterBottom>
+                  <Typography fontWeight='medium' gutterBottom>
                     {item.title}
                   </Typography>
-                  <Typography variant="body2" sx={{ color: 'grey.400' }}>
+                  <Typography variant='body2' sx={{ color: 'grey.400' }}>
                     {item.description}
                   </Typography>
                 </div>
@@ -118,5 +108,5 @@ export default function Highlights() {
         </Grid>
       </Container>
     </Box>
-  );
+  )
 }
